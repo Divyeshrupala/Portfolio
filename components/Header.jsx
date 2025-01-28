02c92e3954d96f41bf1da39d1ab4422ee80047f2
Divyesh Rupala <divyeshrupala789@gmail.com>
2025-01-28 14:38:07 +0530

    final commit

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 function Header() {
   return (
@@ -8,36 +6,31 @@ function Header() {
       <div className="container d-flex align-items-center">
         <h1 className="logo me-auto"><a href="index.html">Divyesh Rupala</a></h1>
         <nav id="navbar" className="navbar">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item"><a className="nav-link scrollto active" href="#hero">Home</a></li>
-            <li className="nav-item"><a className="nav-link scrollto" href="#about">About</a></li>
-            <li className="nav-item"><a className="nav-link scrollto" href="#team">Team</a></li>
-            <li className="nav-item dropdown">
-              <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Skills
-              </a>
-              <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a className="dropdown-item" href="#">C/C++</a>
-                <a className="dropdown-item" href="#">Python</a>
-                <a className="dropdown-item" href="#">PHP</a>
-                <div className="dropdown-divider"></div>
-                <div className="dropdown-submenu">
-                  <a className="dropdown-item dropdown-toggle" href="#">Frontend Development</a>
-                  <div className="dropdown-menu">
-                    <a className="dropdown-item" href="#">Html-5</a>
-                    <a className="dropdown-item" href="#">CSS-3</a>
-                    <a className="dropdown-item" href="#">JavaScript</a>
-                    <a className="dropdown-item" href="#">Bootstrap</a>
-                  </div>
-                </div>
-              </div>
+          <ul>
+            <li><a className="nav-link scrollto active" href="#hero">Home</a></li>
+            <li><a className="nav-link scrollto" href="#about">About</a></li>
+            <li><a className="nav-link scrollto" href="#team">Team</a></li>
+            <li className="dropdown">
+              <a href="#"><span>Skills</span> <i className="bi bi-chevron-down"></i></a>
+              <ul>
+                <li><a href="#">C/C++</a></li>
+                <li className="dropdown">
+                  <a href="#"><span>Frontend Development</span> <i className="bi bi-chevron-right"></i></a>
+                  <ul>
+                    <li><a href="#">Html-5</a></li>
+                    <li><a href="#">CSS-3</a></li>
+                    <li><a href="#">JavaScript</a></li>
+                    <li><a href="#">Bootstrap</a></li>
+                  </ul>
+                </li>
+                <li><a href="#">Python</a></li>
+                <li><a href="#">PHP</a></li>
+              </ul>
             </li>
-            <li className="nav-item"><a className="nav-link scrollto" href="#contact">Contact</a></li>
-            <li className="nav-item"><a className="getstarted scrollto" href="#about">Get Started</a></li>
+            <li><a className="nav-link scrollto" href="#contact">Contact</a></li>
+            <li><a className="getstarted scrollto" href="#about">Get Started</a></li>
           </ul>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
+          <i className="bi bi-list mobile-nav-toggle"></i>
         </nav>
       </div>
     </header>
