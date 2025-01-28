9ab8edf0a2bf6a74bd935a80f4ac3b873c0fad2e
Divyesh Rupala <divyeshrupala789@gmail.com>
2025-01-28 11:17:42 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from '../components/Header';
 import Hero from '../components/Hero';
 import About from '../components/About';
-import Team from '../components/team';
+import Team from '../components/Team';
 import Contact from '../components/Contact';
 import Footer from '../components/Footer';
 import '../assets/css/style.css';
